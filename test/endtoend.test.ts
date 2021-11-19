99239351fa218fcb3cc3fd5798e7148451cbc048
Eric Willhoit <ewillhoit@salesforce.com>
2021-11-19 12:30:48 -0600

    fix: run command in test itself

--- test/endtoend.test.ts
+++ test/endtoend.test.ts
@@ -9,6 +9,7 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import { fs } from '@salesforce/core';
 import { expect } from 'chai';
+import { exec } from 'shelljs';
 
 /**
  * This plugin was primarily made to generate command reference docs for
@@ -27,33 +28,36 @@ function loadTestDitamapFile(path: string) {
 
 describe('salesforcedx', () => {
   before(async () => {
+    exec('./bin/run commandreference:generate --plugins salesforcedx --outputdir test/tmp/');
+
     try {
       await fs.access(testFilesPath);
     } catch (e) {
       throw new Error('Could not read generated test docs. Ensure the "pretest" has run or run it manually.');
     }
   });
+
   after(async () => {
     await fs.remove(testFilesPath);
   });
 
-  it('creates closed-pilot commands', async () => {
+  it('creates closed-pilot commands', () => {
     const dita = loadTestDitamapFile(join('force', 'org', 'cli_reference_force_org_shape_create.xml'));
     expect(/invitation-only\s+pilot\s+program/.test(dita)).to.be.true;
   });
-  it('creates beta commands', async () => {
+  it('creates beta commands', () => {
     const dita = loadTestDitamapFile(join('force', 'org', 'cli_reference_force_org_clone.xml'));
     expect(/a\s+beta\s+version\s+of\s+the/.test(dita)).to.be.true;
   });
-  it('creates open-pilot commands', async () => {
+  it('creates open-pilot commands', () => {
     const dita = loadTestDitamapFile(join('force', 'package', 'cli_reference_force_package_hammertest_run.xml'));
     expect(/through\s+a\s+pilot\s+program\s+that\s+requires/.test(dita)).to.be.true;
   });
-  it('creates with long description', async () => {
+  it('creates with long description', () => {
     const dita = loadTestDitamapFile(join('force', 'source', 'cli_reference_force_source_push.xml'));
     expect(/shortdesc">Pushes changed/.test(dita)).to.be.true;
   });
-  it('creates parameters', async () => {
+  it('creates parameters', () => {
     const dita = loadTestDitamapFile(join('force', 'alias', 'cli_reference_force_alias_list.xml'));
     expect(/<title><ph>Parameters<\/ph><\/title>/.test(dita)).to.be.true;
   });
