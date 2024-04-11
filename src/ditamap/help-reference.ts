4927382afa3e93c80c597ea65c44db6909758a41
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2024-04-11 16:08:55 -0500

    feat!: esm, core7 (#600)
    * chore: updates from devScripts
    
    * feat: esm
    
    * chore: linkinator
    
    * chore: bin/run for generate
    
    * chore: core 7
    
    * test: use bin/dev
    
    * chore: bump plugin-auth
    
    * chore: bump oclif/core, etc
    
    * ci: no ts-noe for cmd-ref
    
    * test: windows ut via node command
    
    * chore: bump plugin-auth for new plugin-info
    
    ---------
    
    Co-authored-by: svc-cli-bot <Svc_cli_bot@salesforce.com>

--- src/ditamap/help-reference.ts
+++ src/ditamap/help-reference.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { Ditamap } from './ditamap';
+import { Ditamap } from './ditamap.js';
 
 export class HelpReference extends Ditamap {
   public constructor() {
